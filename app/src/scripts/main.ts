789d085372606aeac9e2a61aafe532db04078a19
Basma Khalil <basma.mahmoud.khalil@gmail.com>
2024-10-08 18:09:01 +0300

    feat: Add how tere works section

--- app/src/scripts/main.ts
+++ app/src/scripts/main.ts
@@ -1,9 +1,14 @@
 import '../assets/styles/style.scss';
-import { Popover } from 'bootstrap';
+import { Popover, Carousel } from 'bootstrap';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 document.querySelectorAll('[data-bs-toggle="popover"]').forEach((popover) => {
   new Popover(popover);
 });
+
 AOS.init();
+
+const appCarousel = document.querySelector('#carousel') as Element;
+// @ts-ignore:next-line
+const carousel = new Carousel(appCarousel);
